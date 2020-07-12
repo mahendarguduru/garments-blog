bd7e2ac7e90d1faaf2956df61adf97af7c210edc
mahendarguduru <mahendargudurusvss@gmail.com>
2020-07-12 13:12:20 +0530

    first commit

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,7 +4,7 @@ import Card from '../UI/card';
 
 const Layout=(props)=>{
     return(
-        <div style={{textAlign:'center', marginBottom:'20px'}}>
+        <div style={{textAlign:'center', margin:'10px 0'}}>
             <Card style={{borderRadius:'5px'}}>
                 <div className="address">
                 <div style={{width:'30%'}}>
@@ -13,7 +13,7 @@ const Layout=(props)=>{
                         <p style={{margin:'0'}}>Fashion makes Life Worthful</p>
                     </section>
                 </div>
-                <div style={{width:'40%'}}>
+                <div className="companyAddress"style={{width:'40%'}}>
                    <h7>Address:</h7>
                    <p style={{margin:'0', color:'#6c6c6c'}}>H.No. 8-61/1, Plot No. 1, Dwarakanagar, Boduppal, R.R.Dist, Hyderabad-500039.</p>
                    <p style={{margin:'0', color:'#6c6c6c'}}>TELANGANA, INDIA.</p>
